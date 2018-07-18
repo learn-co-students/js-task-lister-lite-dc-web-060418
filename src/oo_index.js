b666ac4186ceddbf9933766b96013454d5165b5a
orenmagid <autoimpedicis@gmail.com>
2018-07-18 17:35:11 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,6 +1,6 @@
 let taskList;
 let Task;
-
+let displayList;
 document.addEventListener("DOMContentLoaded", () => {
   taskList = new TaskList();
   Task = createTask();
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault()
     new Task(document.getElementById('new-task-description').value);
     document.getElementById("create-task-form").reset()
-    let displayList = document.getElementById('tasks')
+    displayList = document.getElementById('tasks')
     displayList.innerHTML = ""
     displayTasks();
   })
@@ -19,12 +19,33 @@ document.addEventListener("DOMContentLoaded", () => {
 function makeListItemFromTask(task) {
   let taskElement = document.createElement("li")
   taskElement.innerHTML = task.description
+  taskElement.id = task.id
   let displayList = document.getElementById('tasks')
   displayList.appendChild(taskElement)
+  createDeleteButton(taskElement, task)
 };
 
 function displayTasks() {
   for (let element of taskList.tasks) {
     makeListItemFromTask(element)
   }
+};
+
+function createDeleteButton(node, task) {
+  let btn = document.createElement("button")
+  btn.innerHTML = "x"
+  btn.id = task.id
+  btn.addEventListener("click", function(event) {
+    for (let element of taskList.tasks) {
+      if (element.id === parseInt(btn.id))
+      // taskList.tasks.splice(taskList.tasks.indexOf(element), 1)
+      // delete taskList.tasks[btn.id - 1]
+      taskList.tasks.splice(parseInt(btn.id) - 1, 1)
+      displayList.innerHTML = ""
+      displayTasks();
+    }
+  })
+
+  node.appendChild(btn)
+
 };
