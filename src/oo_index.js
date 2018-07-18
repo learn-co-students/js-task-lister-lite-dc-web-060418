a378e10f071a96080d07fba3eeed2fa793a1889c
orenmagid <autoimpedicis@gmail.com>
2018-07-18 16:42:00 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,30 @@
+let taskList;
+let Task;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  taskList = new TaskList();
+  Task = createTask();
+
+  document.querySelector("#submit").addEventListener("click", (event) => {
+    event.preventDefault()
+    new Task(document.getElementById('new-task-description').value);
+    document.getElementById("create-task-form").reset()
+    let displayList = document.getElementById('tasks')
+    displayList.innerHTML = ""
+    displayTasks();
+  })
+
 });
+
+function makeListItemFromTask(task) {
+  let taskElement = document.createElement("li")
+  taskElement.innerHTML = task.description
+  let displayList = document.getElementById('tasks')
+  displayList.appendChild(taskElement)
+};
+
+function displayTasks() {
+  for (let element of taskList.tasks) {
+    makeListItemFromTask(element)
+  }
+};
