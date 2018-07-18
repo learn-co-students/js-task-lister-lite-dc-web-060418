370e971543072db74f87c71676e0af7e7b30b62e
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-18 11:20:59 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,27 @@
-class Task {
+function createTask(){
+    let TaskId = 0
+    return class {
+        constructor(description){
+            this.description = description
+            this.id = ++TaskId
+        }
+        render(){
+            const task = document.createElement('li')
+            task.id = this.id
+            task.innerText = this.description
+
+            const deleteButton = document.createElement('button')
+            deleteButton.innerText = "x"
+            deleteButton.addEventListener('click', this.removeItem)
+
+            task.appendChild(deleteButton)
+
+            return task
+        }
+
+        removeItem(event){
+            const clickedTask = event.target.parentElement
+            clickedTask.remove()
+        }
+    }
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,11 @@
 class TaskList {
+    constructor(){
+        this.node = document.querySelector('#tasks')
+        this.Task = createTask()
+    }
+
+    addTask(description){
+        const newTask = new this.Task(description)
+        this.node.appendChild(newTask.render())
+    }
 }
