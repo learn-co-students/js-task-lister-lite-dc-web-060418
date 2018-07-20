6cd0ae24c74b3f2d5a8feb401ca13056596f32de
Essa Hijazi <github email address>
2018-07-20 15:40:44 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  createNewTask(getSubmitButton());
 });
+
+function createNewTask(node) {
+    node.addEventListener('click', function(event) {
+      event.preventDefault();
+      let li = document.createElement('li');
+      li.innerText = getUserInput()
+      getTaskList().appendChild(li);
+    })
+}
+
+
+
+function getSubmitButton() {
+  return document.querySelector('#create-task-form').children[2]
+}
+
+function getTaskList() {
+  return document.getElementById('tasks');
+}
+
+function getUserInput() {
+    return document.getElementById('new-task-description').value;
+}
